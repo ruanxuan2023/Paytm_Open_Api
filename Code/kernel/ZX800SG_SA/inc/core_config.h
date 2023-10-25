06a24b928836e5bd75f3c04fbfb416836eb3a112
CY <1348456148@qq.com>
2023-10-25 14:01:19 +0800

    Update the kernel to support function osiSetPMEnableSleep

--- Code/kernel/ZX800SG_SA/inc/core_config.h
+++ Code/kernel/ZX800SG_SA/inc/core_config.h
@@ -1,11 +1,8 @@
 #ifndef __CORE_CONFIG_H__
-#define __CORE_CONFIG_H__
-
-// Auto generated. Don't edit it manually!
-
-#define CONFIG_RAM_ADDRESS       (0x7e358000)
-#define CONFIG_RAM_SIZE       (0x00007ffc)
-
+#define __CORE_CONFIG_H__
+// Auto generated. Don't edit it manually!
+#define CONFIG_RAM_ADDRESS       (0x7e320000)
+#define CONFIG_RAM_SIZE       (0x0003fffc)
 #define CONFIG_BOARD_FLASH_SIZE       (0x400000)
 #define CONFIG_APP_FLASH_ADDRESS       (0x803d6000)
 #define CONFIG_APP_FLASH_SIZE       (0x20000)
