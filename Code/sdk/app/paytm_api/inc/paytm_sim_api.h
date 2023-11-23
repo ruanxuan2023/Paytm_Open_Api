fd3c89a53d751e43434da2db95407c8460aeb527
CY <1348456148@qq.com>
2023-11-23 14:33:19 +0800

    Paytm_PlayFileFromDir will return fail when where is any audio file missing in play list

--- Code/sdk/app/paytm_api/inc/paytm_sim_api.h
+++ Code/sdk/app/paytm_api/inc/paytm_sim_api.h
@@ -18,12 +18,52 @@ bool PaytmGetAPN(char apn[64]);
 int32 Paytm_ReadSubscriberNumber(char subscriber_number[]);
 //TODO--这个类型无定义
 Paytm_network_provider_t Paytm_GetNetworkProvider(uint16 mcc, uint16 mnc);
+
+/**
+ * @description: Unrealized, please not use it
+ * @return {*}
+ */
 int32 Paytm_ReadMSISDN(const char *ussd, char msisdn[]);
+
+/**
+ * @description: 
+ * @return {*}  0-enjected, 1-inserted
+ */
 int32 Paytm_GetSimState(void);
+
+/**
+ * @description: Register the cb to receive sim&net state
+ * @return {*}
+ */
 int32 Paytm_GetModemFunction(uint8 *func);
+
+/**
+ * @description: Unrealized, please not use it
+ * @return {*}
+ */
 int32 Paytm_ResetSimModule(void);
+
+/**
+ * @description: Read csq value of singal strength
+ * @return {*}   Range from 0 - 23, if return 99, means no signal
+ */
 int32 Paytm_GetSignalStrength(void);
+
+/**
+ * @description: Unrealized, please not use it
+ * @return {*}  
+ */
 int32 Paytm_GetGSMState(int32 *stat);
+
+/**
+ * @description: Register the cb to receive sim&net state. The same as Paytm_GetModemFunction
+ * @return {*}
+ */
 int32 Paytm_SimMonitor(uint8 *func);
+
+/**
+ * @description: Register the cb to receive sim&net state. The same as Paytm_GetModemFunction
+ * @return {*}
+ */
 void sim_state_callback_register(message_callback_t cb);
 #endif
