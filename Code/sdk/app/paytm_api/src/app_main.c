fb3da53e2d8e1fc8361a08a43b3f412cf3eca6a6
CY <1348456148@qq.com>
2023-11-28 17:08:17 +0800

    Fix the bug that when sim card is not ready, device returns network connected event; When the network disconnected, it will return the event only one time

--- Code/sdk/app/paytm_api/src/app_main.c
+++ Code/sdk/app/paytm_api/src/app_main.c
@@ -485,12 +485,13 @@ void app_main(void)
     Paytm_TRACE("************************************************\n");
     // // WM_APP_DOWNLOAD WM_GET_SIM_INFO WM_FILE_TEST WM_CERT_READ_WRITE WM_OTA_TEST_QA WM_PWK_DEMO TEST_MQTT_LOOP_QA
     OpenDemoViaId(WM_PWK_DEMO);
-    // OpenDemoViaId(WM_BUTTON_DEMO);
-
+    OpenDemoViaId(WM_BUTTON_DEMO);
+    OpenDemoViaId(WM_GET_SIM_INFO);
     // Paytm_TRACE("***************TEST FOTA BIN********************\n");
     while (1)
     {
         Paytm_delayMilliSeconds(1000);
+        // Paytm_TRACE("Device %s charging", Paytm_GetChargingStatus() ? "is" : " is not");
     }
 
     return;
